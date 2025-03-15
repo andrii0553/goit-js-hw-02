117b99bfb93a4d753323bcb3333d12f6e0451f2d
andrii0553 <baydak0553@gmail.com>
2025-03-15 13:40:03 +0300

    init

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+
+function checkForSpam(message) {
+    let string = message.toLowerCase();
+    return string.includes('spam') || string.includes('sale');
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+
+
+
+
+
+
+
+
---
+++ js/task-4.js
@@ -0,0 +1,37 @@
+function getShippingCost(country) {
+    
+    switch (country) {
+        case 'China':
+            price = 100;
+            break;
+        case 'Chile':
+            price = 250;
+            break;
+        case 'Australia':
+            price = 170;
+            break;
+        case 'Jamaica':
+            price = 120;
+            break;
+        default:
+            price = 0;
+            break;
+        
+    }
+    if (price === 0) {
+        return '"Sorry, there is no delivery to your country"';
+        
+    } else {
+        return `"Shipping to ${country} will cost ${price} credits"`;
+    }
+}
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden")); 
+
+
+
